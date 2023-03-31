6e59b7b49ca82dcbbd64d7cf5f19c70b92e7da67
tamara_hera <tamara.herashchenko@gmail.com>
2023-03-31 13:48:06 +0200

    update projects structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,39 +3,52 @@ import { useState, useEffect } from 'react';
 import ShopAdd from '../shopAdd/ShopAdd';
 import ShopFilter from '../shopFilter/ShopFilter';
 import ShopFind from '../shopFind/ShopFind';
-import ShopList from '../shopList/ShopList';
+import {ShopList, InitTitle} from '../shopList/ShopList';
 import ShopListItem from '../shopListItem/ShopListItem';
 import ShopTotal from '../shopTotal/ShopTotal';
 
 import './app.scss';
 
 const dataItems = [
-  {shopItem: 'Bananas', 
-  amount: '2', 
-  price: '3',
-  checked: false, 
-  important: false},
-  {shopItem: 'Cat`s food', 
-  amount: '5', 
-  price: '10',
-  checked: false, 
-  important: true},
-  {shopItem: 'Bread', 
-  amount: '4', 
-  price: '8',
-  checked: false, 
-  important: false},
-  {shopItem: 'Milk', 
-  amount: '3', 
-  price: '6',
-  checked: true, 
-  important: false}
+  {
+    name: 'Bananas', 
+    amount: '2', 
+    price: '3',
+    checked: false, 
+    important: false,
+    id: '1'
+  },
+  {
+    name: 'Cat`s food', 
+    amount: '5', 
+    price: '10',
+    checked: false, 
+    important: true,
+    id: '2'
+  },
+  {
+    name: 'Bread', 
+    amount: '4', 
+    price: '8',
+    checked: false, 
+    important: false,
+    id: '3'
+  },
+  {
+    name: 'Milk', 
+    amount: '3', 
+    price: '6',
+    checked: true, 
+    important: false,
+    id: '4'
+  }
 ];
 
 
 const App = () => {
   const [data, setData] = useState(dataItems);
 
+  console.log(data)
   return (
     <div className='app'>
       <header>
@@ -46,7 +59,9 @@ const App = () => {
           <ShopFind/>
           <ShopFilter/>
         </section>
-        <ShopList data={data}/>        
+        <section className='list'>
+          {data ? <ShopList data={data}/> : <InitTitle/>}
+        </section>
         <ShopAdd/>
         <ShopTotal/>
       </main>
